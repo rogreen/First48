82563fd5236131c53286c4e304f80558e3575576
Robert Green <rogreen@microsoft.com>
2018-02-21 09:49:17 -0800

    First version of app

---
+++ First48/First48/First48/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using First48.Views;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace First48
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            AppCenter.Start("android=c17107c7-8ea5-4e65-b806-874e33206753;" + 
+                "uwp={Your UWP App secret here};" + 
+                "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
