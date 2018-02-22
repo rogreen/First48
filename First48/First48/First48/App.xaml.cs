586192f74f5e33ac6aa1bdb0fc28e812fb78ccff
Robert Green <rogreen@microsoft.com>
2018-02-22 15:16:50 -0800

    Fixed push notifications

--- First48/First48/First48/App.xaml.cs
+++ First48/First48/First48/App.xaml.cs
@@ -22,34 +22,13 @@ namespace First48
 
 		protected override async void OnStart ()
 		{
-            // This should come before AppCenter.Start() is called
-            Push.PushNotificationReceived += (sender, e) => {
-
-                // Add the notification message and title to the message
-                var summary = $"Push notification received:" +
-                                    $"\n\tNotification title: {e.Title}" +
-                                    $"\n\tMessage: {e.Message}";
-
-                // If there is custom data associated with the notification,
-                // print the entries
-                if (e.CustomData != null)
-                {
-                    summary += "\n\tCustom data:\n";
-                    foreach (var key in e.CustomData.Keys)
-                    {
-                        summary += $"\t\t{key} : {e.CustomData[key]}\n";
-                    }
-                }
-
-                // Send the notification summary to debug output
-                System.Diagnostics.Debug.WriteLine(summary);
-            };
-
             // Handle when your app starts
             AppCenter.Start("android=c17107c7-8ea5-4e65-b806-874e33206753;" + 
                 "uwp={Your UWP App secret here};" + 
                 "ios={Your iOS App secret here}", 
                 typeof(Analytics), typeof(Crashes), typeof(Push));
+
+            bool isEnabled = await Push.IsEnabledAsync();
         }
 
 		protected override void OnSleep ()
